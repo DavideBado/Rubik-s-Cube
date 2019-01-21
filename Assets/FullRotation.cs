27bfc5ebadecb8959621a49b866ba71f0dc113e3
DavideBado <dabado2510@gmail.com>
2019-01-21 12:46:19 +0100

    Ora è possibile ruotare il cubo con le frecce direzionali, è altresì possibile capire quale fila sia selezionata senza dover visualizzare i trigger (quest'ultima modifica genera errori, per maggiori informazioni leggere il commento nello script dei cubi)

---
+++ Assets/FullRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullRotation : MonoBehaviour {
+    float Input_X, Input_Y;   
+  
+    // Update is called once per frame
+    void Update () {
+        GiveMeATurboSpin();
+
+        // Help me Lord, I'm lost, I want my mommy point
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.transform.rotation = Quaternion.identity;
+        }
+	}
+
+    void GiveMeATurboSpin()
+    {
+        Input_X = Input.GetAxis("Horizontal");
+        Input_X *= Time.deltaTime;
+        Input_Y = Input.GetAxis("Vertical");
+        Input_Y *= Time.deltaTime;
+
+        if(Input_X != 0)
+        {
+            this.transform.Rotate(new Vector3(0, 1f));
+        }
+        if (Input_Y != 0)
+        {
+            this.transform.Rotate(new Vector3(1f, 0));
+        }
+    }
+}
+
