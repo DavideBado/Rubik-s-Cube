4908edf6710bd311fe9f64a17fc18406d6888aff
DavideBado <dabado2510@gmail.com>
2019-01-21 14:50:41 +0100

    Solo un aggiornamento al comento nello script dei cubi

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -18,7 +18,8 @@ public class CubeScript : MonoBehaviour {
     void Update()
     {
         // Queste righe di codice permettono di capire quale fila è selezionata, ma cercando il GameObject Trigger in ogni update segnala una riga di errori,
-        // ho provato a spostare il rend.sharedMaterial[0] in un TriggerExit, ma non lo legge....
+        // ho provato a spostare il rend.sharedMaterial[0] in un TriggerExit, ma non lo legge....creando una funzione e richiamandola nello script dei trigger
+        // le cose migliorano, ma evidenzia un solo cubo e non la fila, ho provato con i foreach, ma non sono riuscito a farli funzionare come volevo
         TriggerCheck = Trigger.GetComponent<Collider>().enabled;
         if (TriggerCheck == true)
         {
